7c23bdb997a0a194ddbf0d28dca0cb851ee11f4a
varavin1 <varavin1@LIN73001567.corp.capgemini.com>
2022-01-19 01:20:50 +0530

    Application TC

--- BugManagement/src/test/java/com/bugtracking/bug/BugApplicationTests.java
+++ BugManagement/src/test/java/com/bugtracking/bug/BugApplicationTests.java
@@ -1,13 +1,15 @@
 package com.bugtracking.bug;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BugApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
-
 }
